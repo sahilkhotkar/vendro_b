eb5f190dea5ba8b2010181aacca84c063435cd21
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2026-01-03 21:33:34 +0530

    config

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'vendro',
+  user: process.env.DB_USER || 'u217412984_vendro',
+  password: process.env.DB_PASSWORD || 'Vendro@123',
+  database: process.env.DB_NAME || 'u217412984_vendro',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
