5794f35b7aeb10248618cadd1de392c7d4a07da7
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2026-01-04 14:43:17 +0530

    host change

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '193.203.184.109',
   user: process.env.DB_USER || 'u217412984_vendro',
   password: process.env.DB_PASSWORD || 'Vendro@123',
   database: process.env.DB_NAME || 'u217412984_vendro',
